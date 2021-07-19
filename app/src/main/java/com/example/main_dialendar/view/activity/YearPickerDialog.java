4b2ebd0e58b3b20ef2648beb12e7967ae06fe9a0
raae7742 <aeae_1@naver.com>
2021-07-19 14:19:18 +0900

    [FEAT] yearDialog 레이아웃

---
+++ app/src/main/java/com/example/main_dialendar/view/activity/YearPickerDialog.java
@@ -0,0 +1,67 @@
+package com.example.main_dialendar.view.activity;
+
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.NumberPicker;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.example.main_dialendar.R;
+
+import java.util.Calendar;
+
+public class YearPickerDialog extends DialogFragment {
+
+    private static final int MAX_YEAR = 2030;
+    private static final int MIN_YEAR = 1980;
+
+    private DatePickerDialog.OnDateSetListener listener;
+    public Calendar cal = Calendar.getInstance();
+
+    public void setListener(DatePickerDialog.OnDateSetListener listener){
+        this.listener = listener;
+    }
+
+    Button btn_ok;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        View dialog = inflater.inflate(R.layout.dialog_year, null);
+
+        btn_ok = dialog.findViewById(R.id.btn_ok);
+
+        final NumberPicker yearPicker = (NumberPicker) dialog.findViewById(R.id.picker_year);
+        final NumberPicker monthPicker = (NumberPicker) dialog.findViewById(R.id.picker_month);
+
+        btn_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onDateSet(null, yearPicker.getValue(), monthPicker.getValue(), 0);
+                YearPickerDialog.this.getDialog().cancel();
+            }
+        });
+
+        monthPicker.setMinValue(1);
+        monthPicker.setMaxValue(12);
+        monthPicker.setValue(cal.get(Calendar.MONTH) + 1);
+
+        int year = cal.get(Calendar.YEAR);
+        yearPicker.setMinValue(MIN_YEAR);
+        yearPicker.setMaxValue(MAX_YEAR);
+        yearPicker.setValue(year);
+
+        builder.setView(dialog);
+
+        return builder.create();
+    }
+}
