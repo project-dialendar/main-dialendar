8324573c6e506c67b9d5d85ba2ec5a80c207b3f9
raae7742 <aeae_1@naver.com>
2021-09-14 22:13:04 +0900

    [FIX} 가로모드 고정, 액션바 제거

--- app/src/main/java/com/example/main_dialendar/view/activity/MainActivity.java
+++ app/src/main/java/com/example/main_dialendar/view/activity/MainActivity.java
@@ -18,6 +18,7 @@ import android.widget.DatePicker;
 import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
@@ -29,7 +30,19 @@ import com.example.main_dialendar.R;
 import com.example.main_dialendar.view.adapter.CalendarAdapter;
 import com.example.main_dialendar.view.adapter.WeekAdapter;
 import com.example.main_dialendar.view.dialog.YearPickerDialog;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -70,6 +83,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     // 캘린더 변수
     private Calendar mCal;
 
+    // 구글 로그인 상수 & 변수
+    private static final int SIGN_IN = 9001;
+    GoogleSignInClient client;
+    private FirebaseAuth mAuth;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -81,15 +100,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         gv_month = findViewById(R.id.gv_month);
         gv_day_of_week = findViewById(R.id.gv_day_of_week);
 
-        btn_year = findViewById(R.id.btn_year);
+        btn_year = (Button)findViewById(R.id.btn_year);
         btn_year.setOnClickListener(this);
 
-        btn_write = findViewById(R.id.btn_write);
+        btn_write = (ImageButton)findViewById(R.id.btn_write);
         btn_write.setOnClickListener(this);
 
         drawerLayout = findViewById(R.id.drawerLayout);
 
         navigationView = findViewById(R.id.navigationView);
+
+        // 구글 로그인 옵션 설정
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        client = GoogleSignIn.getClient(this, gso);
+        mAuth = FirebaseAuth.getInstance();
+
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -104,12 +132,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     case (R.id.mail) :
                         sendEmailToAdmin("[일력 문의사항]", new String[]{"apps@gmail.com"});
                         break;
+                    case (R.id.login) :
+                        signIn();
+                        break;
                 }
                 return true;
             }
         });
 
         // 상단 툴바 설정
+        setToolbar();
+    }
+
+    private void setToolbar() {
         setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
         getSupportActionBar().setDisplayShowCustomEnabled(true);    // 커스터마이징을 위해 필요
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);      // 툴바 메뉴 버튼 생성
@@ -131,6 +166,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         gv_day_of_week.setAdapter(day_of_weekGridAdapter);
 
         dayList = new ArrayList<Day>();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
     }
 
     @Override
@@ -251,5 +294,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 "\n기기명(Device): \n안드로이드 OS(Android OS): \n내용(Content): \n");
         email.setType("message/rfc822");
         startActivity(email);
+    }
+
+    private void signIn() {
+        Intent signInIntent = client.getSignInIntent();
+        startActivityForResult(signInIntent, SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                Toast.makeText(this, "Failed Google Login", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) {
+
     }
 }
