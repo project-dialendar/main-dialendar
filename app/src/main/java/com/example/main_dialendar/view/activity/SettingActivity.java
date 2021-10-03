41d19ccd23243507c37f1769273111966df2a9b2
raae7742 <aeae_1@naver.com>
2021-10-03 12:52:16 +0900

    [FIX] 설정 화면 Toolbar 오류

--- app/src/main/java/com/example/main_dialendar/view/activity/SettingActivity.java
+++ app/src/main/java/com/example/main_dialendar/view/activity/SettingActivity.java
@@ -26,27 +26,5 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
-
-        // 상단 툴바 설정
-        setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
-        getSupportActionBar().setDisplayShowCustomEnabled(true);    // 커스터마이징을 위해 필요
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);      // 툴바 메뉴 버튼 생성
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.btn_back); // 메뉴 버튼 모양 설정
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FFFFFF")));
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-
-    }
-
-    // 툴바에 있는 옵션을 선택한 경우
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home: {
-                // 뒤로 가기
-                startActivity(new Intent(this, MainActivity.class));
-            }
-            default:
-                return super.onOptionsItemSelected(item);
-        }
     }
 }
