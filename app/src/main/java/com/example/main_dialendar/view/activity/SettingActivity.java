44c48d754e14373388062f2659c8f4fa725862d3
raae7742 <aeae_1@naver.com>
2021-10-03 13:53:47 +0900

    [FEAT] 설정화면 뒤로 가기 버튼

--- app/src/main/java/com/example/main_dialendar/view/activity/SettingActivity.java
+++ app/src/main/java/com/example/main_dialendar/view/activity/SettingActivity.java
@@ -13,6 +13,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.example.main_dialendar.R;
@@ -22,9 +23,20 @@ import com.example.main_dialendar.R;
  */
 public class SettingActivity extends AppCompatActivity {
 
+    Button btn_back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        btn_back = findViewById(R.id.btn_back);
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SettingActivity.this, MainActivity.class));
+            }
+        });
     }
 }
